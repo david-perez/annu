6ce9025caf6309c9284e4ae5172268f95a67ad22
Marco Antonio Garrido Rojo <MaSteve@users.noreply.github.com>
2017-11-07 15:29:06 +0100

    Corrector-Predictor (Euler + Trapecio), Van der Pol

---
+++ eulertr/mieulertr.m
@@ -0,0 +1,17 @@
+function [t, u, numfun] = mieulertr(t0, tfin, N, x0, f, J)
+    h = (tfin - t0) / N;
+    t = [t0:h:tfin];
+    numfun = 0; % TODO
+    m = size(x0, 1);
+    
+    u = zeros(m, N + 1);
+    u(:, 1) = x0;
+    for n = 1:N
+        % Predigo
+        z = u(:, n) + h * feval(f, t(n), u(:, n));
+        for j = 1:J % Corrijo
+            z = u(:, n) + h / 2 * (feval(f, t(n), u(:, n)) + feval(f, t(n + 1), z));
+        end
+        u(:, n + 1) = z;
+    end
+end
---
+++ functions/funcvanderpol.m
@@ -0,0 +1,5 @@
+function f = funcvanderpol(t, x)
+    alfa = 1;
+    beta = 1;
+    f = [x(2); -x(1) - alfa*(x(1)*x(1) - beta)*x(2)];
+end
