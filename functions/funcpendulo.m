16008cb8de994b41181ad590f15f2219059c1bcc
Marco Antonio Garrido Rojo <3d17marcoantonio.garrido@gmail.com>
2017-12-30 21:35:49 +0100

    Add funcpendulolin and funcpendulo

---
+++ functions/funcpendulo.m
@@ -0,0 +1,8 @@
+function f = funcpendulo(t, x)
+    % theta''(t) + 2*beta*theta'(t) + (g/l)*sin(theta(t)) = 0
+    beta = 0; % Parámetro de resistencia.
+    g = 9.8; % Gravedad.
+    l = 1; % Longitud del péndulo.
+    M = 0;
+    f = [x(2); M - (g / l) * sin(x(1)) - 2 * beta * x(2)];
+end
---
+++ functions/funcpendulolin.m
@@ -0,0 +1,7 @@
+function f = funcpendulolin(t, x)
+    % theta''(t) + 2*beta*theta'(t) + (g/l)*theta(t) = 0
+    beta = 0; % Parámetro de resistencia.
+    g = 9.8; % Gravedad.
+    l = 1; % Longitud del péndulo.
+    f = [x(2); - (g / l) * x(1) - 2 * beta * x(2)];
+end
