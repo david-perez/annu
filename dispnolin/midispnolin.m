6f3455dde9ff03084a1a44cc3de1d68d3e3d55fa
Marco Antonio Garrido Rojo <3d17marcoantonio.garrido@gmail.com>
2018-01-01 21:40:12 +0100

    Modify midispnolin.m for different boundary conditions

--- dispnolin/midispnolin.m
+++ dispnolin/midispnolin.m
@@ -1,14 +1,33 @@
-function [t, x] = midispnolin(ffxfy, t0, T, N, a, b, TOL)
+function [t, x] = midispnolin(ffxfy, t0, T, N, a, b, TOL, C1, C2)
     itmax = 10000;
-    sk = (b - a) / (T - t0);
+    if C1 == 0 && C2 ==0
+        sk = (b - a) / (T - t0);
+    elseif C1 == 0 && C2 == 1
+        sk = b;
+    else
+        sk = b - a * (T - t0);
+    end
     for k = 1:itmax
-        x0 = [a;sk;0;1];
+        if C1 == 0
+            x0 = [a;sk;0;1];
+        else
+            x0 = [sk;a;0;1];
+        end
         [t, xk] = mirk4(t0, T, N, x0, @fundispnolin, ffxfy);
-        if norm(xk(1, N + 1) - b) < TOL
+        if C2 == 0
+            cond = xk(1, N + 1) - b;
+        else
+            cond = xk(2, N + 1) - b;
+        end
+        if norm(cond) < TOL
             x = xk(1:2, :);
             break;
         end
-        sk = sk - (xk(1, N + 1) - b) / xk(3, N + 1);
+        if C2 == 0
+            sk = sk - (xk(1, N + 1) - b) / xk(3, N + 1);
+        else
+            sk = sk - (xk(2, N + 1) - b) / xk(4, N + 1);
+        end
     end
    
 end
--- dispnolin/testmidispnolin.m
+++ dispnolin/testmidispnolin.m
@@ -5,5 +5,7 @@ T = 2;
 a = 1/2;
 b = 1/3;
 TOL = 1e-6;
-[t, u] = midispnolin(ffxfy, t0, T, N, a, b, TOL);
+C1 = 0;
+C2 = 0;
+[t, u] = midispnolin(ffxfy, t0, T, N, a, b, TOL, C1, C2);
 misgraficasdisp
