97bfe1ceebb23f25f312f5450f9054befa61b1fa
david-perez <hallaplay835@gmail.com>
2018-01-10 18:38:56 +0100

    Fix typo in funcpendulo

--- functions/funcpendulo.m
+++ functions/funcpendulo.m
@@ -1,5 +1,5 @@
 function f = funcpendulo(t, x, ~)
-    % Práctica 2.9.
+    % Práctica 2.9
     % theta''(t) + 2*beta*theta'(t) + (g/l)*sin(theta(t)) = M
     beta = 0; % Parámetro de resistencia.
     g = 9.8; % Gravedad.
