57a8b5fc4bfef1d6bdbbd2e6a263b15038dee395
Marco Antonio Garrido Rojo <3d17marcoantonio.garrido@gmail.com>
2018-01-20 21:51:00 +0100

    Bug fixing in Richardson extrapolation

--- metadap/mimetadap.m
+++ metadap/mimetadap.m
@@ -12,7 +12,7 @@ function [t, u] = mimetadap(t0, tfin, x0, h0, hmin, TOL, f, metodo, orden, hmax,
         
         % Cálculo del error norm(dos saltos - un salto).
         [~, y] = feval(metodo, t(i), t(i) + h, 2, u(:, i), f, par);
-        ERR = norm(z(:,2) - y(:,3)) / h; % Esto debería hacerse con las phis.
+        ERR = norm(z(:,2) - y(:,3)) / (h * (1 - 1 / (2 ^ orden))); % Esto debería hacerse con las phis.
         
         if (ERR <= TOL)
             t = [t, t(i) + h];
